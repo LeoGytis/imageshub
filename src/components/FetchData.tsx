95acaa0e74b17082b531d7fefea90bd113c1480f
leogytis <leogytis@gmail.com>
2024-03-12 07:56:55 +0200

    save for now

--- src/components/FetchData.tsx
+++ src/components/FetchData.tsx
@@ -11,36 +11,20 @@ interface PhotoProps {
 }
 
 const FetchData: React.FC = (): JSX.Element => {
-	const [photos, setPhotos] = useState<PhotoProps[] | null>(null);
+	const [photos, setPhotos] = useState<PhotoProps[]>([]);
 	const [isLoading, setIsLoading] = useState<boolean>(true);
 	const [favorites, setFavorites] = useState<Record<string, boolean>>({});
 
 	// const apiKey = "a38a46fe5bac997c4fdde47d6b7ed5bf";
 	const apiKey = "164c38fb43c193481ea2a3dfc30b4180"; //public
-	// const galleryId = "195820781-72157721014962461"; // big macro gallery
-	const galleryId = "91216181-72157638326919233";
+	const galleryId = "195820781-72157721014962461";
+	// big macro gallery
+	// const galleryId = "91216181-72157638326919233";
 
 	useEffect(() => {
-		const fetchData = async (): Promise<void> => {
-			try {
-				const response = await fetch(
-					`https://www.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=${apiKey}&gallery_id=${galleryId}&format=json&nojsoncallback=1&extras=owner_name`
-				);
-				const responseData = await response.json();
-				if (responseData && responseData.photos && responseData.photos.photo) {
-					setPhotos(responseData.photos.photo);
-				}
-				setIsLoading(false);
-			} catch (error) {
-				console.error("Error fetching data:", error);
-				setIsLoading(false);
-			}
-		};
-
-		fetchData();
+		fetchPhotos();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
-
-	console.log("🚀 :: photos ::", photos);
 
 	useEffect(() => {
 		const storedFavorites = localStorage.getItem("favorites");
@@ -48,6 +32,23 @@ const FetchData: React.FC = (): JSX.Element => {
 			setFavorites(JSON.parse(storedFavorites));
 		}
 	}, []);
+
+	const fetchPhotos = async (): Promise<void> => {
+		setIsLoading(true);
+		try {
+			const response = await fetch(
+				`https://www.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=${apiKey}&gallery_id=${galleryId}&format=json&nojsoncallback=1&extras=owner_name`
+			);
+			const responseData = await response.json();
+			if (responseData && responseData.photos && responseData.photos.photo) {
+				setPhotos((prevPhotos) => [...prevPhotos, ...responseData.photos.photo]);
+			}
+		} catch (error) {
+			console.error("Error fetching data:", error);
+		} finally {
+			setIsLoading(false);
+		}
+	};
 
 	const toggleFavorite = (photoId: string): void => {
 		setFavorites((prevFavorites) => ({
@@ -58,29 +59,25 @@ const FetchData: React.FC = (): JSX.Element => {
 
 	return (
 		<>
-			{isLoading ? (
-				<LoaderComponent />
-			) : (
-				<div className="gallery_wrapper">
-					{photos &&
-						photos.map((photo, i) => (
-							<div className="photo_container" key={i}>
-								<img
-									key={photo.id}
-									alt={photo.title}
-									src={`https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`}
-								/>
-								<div className="overlay">
-									<h3>{photo.title}</h3>
-									<p>{photo.ownername}</p>
-									<button onClick={() => toggleFavorite(photo.id)}>
-										{favorites[photo.id] ? "Unfavorite" : "Favorite"}
-									</button>
-								</div>
-							</div>
-						))}
-				</div>
-			)}
+			<div className="gallery_wrapper">
+				{photos.map((photo, i) => (
+					<div className="photo_container" key={i}>
+						<img
+							key={photo.id}
+							alt={photo.title}
+							src={`https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`}
+						/>
+						<div className="overlay">
+							<h3>{photo.title}</h3>
+							<p>{photo.ownername}</p>
+							<button onClick={() => toggleFavorite(photo.id)}>
+								{favorites[photo.id] ? "Unfavorite" : "Favorite"}
+							</button>
+						</div>
+					</div>
+				))}
+				{isLoading && <LoaderComponent />}
+			</div>
 		</>
 	);
 };
